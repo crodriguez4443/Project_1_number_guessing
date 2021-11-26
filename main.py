1d0ea0a5dd8798a06838017a2a04b4f3efb195fa
Chris Rodriguez <Crodriguez4443@gmail.com>
2021-11-26 16:17:04 -0500

    Update main.py
    Submitted code
    issues: upon first attempt, if a string is given instead of integer (as required), a ValueError breaks the script. Can't fix this.
    
    Also, the "if guess == random_number:" lines of code are repeated. I should probably create a def for this.

--- main.py
+++ main.py
@@ -7,44 +7,60 @@ from statistics import median
 from statistics import mean
 from statistics import mode
 
-print("welcome message")  # generic welcome message
+print("welcome to my game")  # welcome message
 
 
 def start_game():
     random_number = random.randint(0, 10)
     print(random_number)
     guess = int(input("guess a number between 0 and 10. "))
-    if guess != random_number:
-        while guess != random_number:
-            attempts = 0
-            guess_list = []
-            if guess < random_number:
+    attempts = 0
+    guess_list = [guess]
+
+    if guess == random_number:
+        print(f"That's correct! it only took you {attempts} attempts!")
+        print(f"mean: N/A, median: N/A, mode: N/A")
+        play_again = input("would you like to tray again? Y/N")
+        play_again.lower()
+        if play_again == "y":
+            print("sure, lets get started")
+            start_game()
+        elif play_again == "n":
+            print("thanks for playing!")
+        else:
+            print("please try again" )
+    try:
+        while int(guess) != random_number:
+            if 0 < guess < random_number:
                 guess = int(input(f"your guess of {guess} is lower than the number. Try again. "))
-                guess_list.append(attempts)
+                guess_list.append(guess)
+                attempts += 1
                 print(f"here are your guesses so far: {guess_list}")
                 continue
-            elif guess > random_number:
+            elif random_number < guess < 11:
                 guess = int(input(f"your guess of {guess} is higher than the number. Try again. "))
                 guess_list.append(guess)
+                attempts += 1
+                print(f"here are your guesses so far: {guess_list}")
                 continue
-    if guess == random_number:
-        while guess == random_number:
-            print(f"That's correct! it only took you {attempts} attempts!")
-            mean_guess = mean(guess_list)
-            median_guess = median(guess_list)
-            mode_guess = mode(guess_list)
-            print(f"mean: {mean_guess}, median: {median_guess}, mode: {mode_guess}")
-            play_again = input("would you like to tray again? Y/N")
-            play_again.lower()
-            if play_again == "y":
-                print("sure, lets get started")
-                start_game()
-            elif play_again == "n":
-                print("thanks for playing!")
-                break
             else:
-                input("sorry that's not a valid response. try again ")
+                raise ValueError
+    except ValueError:
+        guess = int(input("""Provide a whole number between "0" and "10".""" ))
 
+    if guess == random_number:
+        print(f"That's correct! it only took you {attempts} attempts!")
+        guess_mean = float(mean(guess_list))
+        guess_median = float(median(guess_list))
+        guess_mode = mode(guess_list)
+        print(f"mean: {guess_mean}, median: {guess_median}, mode: {guess_mode}")
+        play_again = input("would you like to tray again? Y/N")
+        play_again.lower()
+        if play_again == "y":
+            print("sure, lets get started")
+            start_game()
+        elif play_again == "n":
+            print("thanks for playing!")
 
 
 
@@ -63,10 +79,10 @@ def start_game():
       b. If the guess is less than the solution, display to the player "It's higher". DONE
 
     4. Once the guess is correct, stop looping, inform the user they "Got it" DONE
-         and show how many attempts it took them to get the correct number.
-    5. Save their attempt number to a list.
-    6. At the end of the game, show the player, 1) their number of attempts, 2) the mean, median, and mode of the saved attempts list.
-    7. Ask the player if they want to play again.
+         and show how many attempts it took them to get the correct number. DONE
+    5. Save their attempt number to a list. DONE
+    6. At the end of the game, show the player, 1) their number of attempts, 2) the mean, median, and mode of the saved attempts list. DONE
+    7. Ask the player if they want to play again. DONE
 
     ( You can add more features/enhancements if you'd like to. )
     """
