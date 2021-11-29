89694d671efb019d767e83c6490c5015a6422876
Chris Rodriguez <Crodriguez4443@gmail.com>
2021-11-29 15:21:27 -0500

    Missing value error for string
    Unable to handle value error if provided a string instead of an integer

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Project_1_number_guessing
  Treehouse project 1
+
+The goal of this game is to guess a randomly generated number between 0 and 10. If an incorrect value is given, the program will direct the user to provide a correct value for their guess. 
+
+This project includes the use of the following features:
+
+For loops
+
+While loops
+
+Lists (appending and calling)
+
+Math: Min, mean, mode, median
+
+formatting
+
+and other features
--- main.py
+++ main.py
@@ -8,12 +8,16 @@ from statistics import mean
 from statistics import mode
 
 print(">>>>>Guess the Magic Number<<<<<")  # welcome message
+scoreboard = [10]
+high_score = min(scoreboard)
+print(f"The current high score is {high_score}")
+
 
 def start_game():
     random_number = random.randint(0, 10)
     print(random_number)
-    guess = int(input("guess a number between 0 and 10. "))
     attempts = 1
+    guess = int(input("guess a number between 0 and 10. "))
     guess_list = [guess]
     while True:
         try:
@@ -30,19 +34,23 @@ def start_game():
                 attempts += 1
                 print(f"here are your guesses so far: {guess_list}")
                 print(f"Number of attempts: {attempts}")
-                # continue?
             elif random_number < guess < 11:
                 guess = int(input(f"your guess of {guess} is higher than the number. Try again. "))
                 guess_list.append(guess)
                 attempts += 1
                 print(f"here are your guesses so far: {guess_list}")
                 print(f"Number of attempts: {attempts}")
-                # continue?
             else:
                 raise ValueError
         except ValueError:
-            print("""Provide a whole number between "0" and "10".""")
+            guess = int(input("""Provide a whole number between "0" and "10". """))
     while True:
+        if attempts < min(scoreboard):
+            scoreboard.append(attempts)
+            print("NEW HIGH SCORE!!!")
+            print(f"the new high score is {min(scoreboard)}")
+        else:
+            print(f"The current high score is still {min(scoreboard)}")
         play_again = input("would you like to tray again? Y/N ")
         play_again.lower()
         if play_again == "y":
@@ -53,35 +61,5 @@ def start_game():
             break
         else:
             print("try again!")
-
-
-
-
-
-
-
-
-
-    """Psuedo-code Hints
-
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player. DONE
-    2. Store a random number as the answer/solution. DONE
-    3. Continuously prompt the player for a guess. DONE
-      a. If the guess greater than the solution, display to the player "It's lower". DONE
-      b. If the guess is less than the solution, display to the player "It's higher". DONE
-
-    4. Once the guess is correct, stop looping, inform the user they "Got it" DONE
-         and show how many attempts it took them to get the correct number. DONE
-    5. Save their attempt number to a list. DONE
-    6. At the end of the game, show the player, 1) their number of attempts, 2) the mean, median, and mode of the saved attempts list. DONE
-    7. Ask the player if they want to play again. DONE
-
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
-#no changes made
-# Kick off the program by calling the start_game function.
 
 start_game()
