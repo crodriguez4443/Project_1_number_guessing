b65c9e986ed01d5b620b5050eeb299b6fe57c960
Chris Rodriguez <Crodriguez4443@gmail.com>
2021-12-03 14:30:18 -0500

    Fixed while loop
    Fixed initial while loop
    All guesses accounted for in Try loop

--- main.py
+++ main.py
@@ -16,34 +16,35 @@ print(f"The current high score is {high_score}")
 def start_game():
     random_number = random.randint(0, 10)
     print(random_number)
-    attempts = 1
-    guess = int(input("guess a number between 0 and 10. "))
-    guess_list = [guess]
+    attempts = 0
+    guess_list = []
     while True:
         try:
-            if guess == random_number:
-                print(f"That's correct! it only took you {attempts} attempts!")
-                guess_mean = float(mean(guess_list))
-                guess_median = float(median(guess_list))
-                guess_mode = mode(guess_list)
-                print(f"mean: {guess_mean}, median: {guess_median}, mode: {guess_mode}")
-                break
-            elif 0 < guess < random_number:
-                guess = int(input(f"your guess of {guess} is lower than the number. Try again. "))
-                guess_list.append(guess)
+            guess = int(int(input("guess a number between 0 and 10. ")))
+            if -1 < guess < 11:
                 attempts += 1
-                print(f"here are your guesses so far: {guess_list}")
-                print(f"Number of attempts: {attempts}")
-            elif random_number < guess < 11:
-                guess = int(input(f"your guess of {guess} is higher than the number. Try again. "))
                 guess_list.append(guess)
-                attempts += 1
-                print(f"here are your guesses so far: {guess_list}")
-                print(f"Number of attempts: {attempts}")
+                if guess == random_number:
+                    print(f"That's correct! it only took you {attempts} attempts!")
+                    guess_mean = float(mean(guess_list))
+                    guess_median = float(median(guess_list))
+                    guess_mode = mode(guess_list)
+                    print(f"mean: {guess_mean}, median: {guess_median}, mode: {guess_mode}")
+                    break
+                elif -1 < guess < random_number:
+                    print(f"your guess of {guess} is lower than the number. Try again. ")
+                    print(f"here are your guesses so far: {guess_list}")
+                    print(f"Number of valid attempts: {attempts}")
+                    continue
+                elif random_number < guess < 11:
+                    print(f"your guess of {guess} is higher than the number. Try again. ")
+                    print(f"here are your guesses so far: {guess_list}")
+                    print(f"Number of valid attempts: {attempts}")
+                    continue
             else:
                 raise ValueError
         except ValueError:
-            guess = int(input("""Provide a whole number between "0" and "10". """))
+            print("""Provide a whole number between "0" and "10". """)
     while True:
         if attempts < min(scoreboard):
             scoreboard.append(attempts)
