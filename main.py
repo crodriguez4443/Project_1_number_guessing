b15efb1227ed97ffe241feb947bc7a62404cfbb0
Chris Rodriguez <Crodriguez4443@gmail.com>
2021-12-09 00:10:14 -0500

    Added high score at beginning of game
    Created
    
    scoreboard = [ ]
    Change mean, median, mode from guess list to high scores.
    
    other small improvements to keep things working.
    
    looks like the final release. from here it's mostly aesthetics

--- main.py
+++ main.py
@@ -8,28 +8,33 @@ from statistics import mean
 from statistics import mode
 
 print(">>>>>Guess the Magic Number<<<<<")  # welcome message
-scoreboard = [10]
-high_score = min(scoreboard)
-print(f"The current high score is {high_score}")
+scoreboard = []
 
 
 def start_game():
     random_number = random.randint(0, 10)
-    print(random_number)
+    try:
+        print(f">>The current high score is {min(scoreboard)}<<")
+    except ValueError:
+        print("The current high score is N/A \n")
+
+    #print(random_number)   <-- these are cheat codes
     attempts = 0
     guess_list = []
     while True:
         try:
-            guess = int(int(input("guess a number between 0 and 10. ")))
+            guess = int(input("guess a number between 0 and 10. "))
             if -1 < guess < 11:
                 attempts += 1
                 guess_list.append(guess)
                 if guess == random_number:
                     print(f"That's correct! it only took you {attempts} attempts!")
-                    guess_mean = float(mean(guess_list))
-                    guess_median = float(median(guess_list))
-                    guess_mode = mode(guess_list)
-                    print(f"mean: {guess_mean}, median: {guess_median}, mode: {guess_mode}")
+                    scoreboard.append(attempts)
+                    if attempts < min(scoreboard):
+                        print("NEW HIGH SCORE!!!")
+                        print(f"the new high score is {min(scoreboard)}")
+                    else:
+                        print(f"The current high score is {min(scoreboard)}")
                     break
                 elif -1 < guess < random_number:
                     print(f"your guess of {guess} is lower than the number. Try again. ")
@@ -44,23 +49,22 @@ def start_game():
             else:
                 raise ValueError
         except ValueError:
-            print("""Provide a whole number between "0" and "10". """)
+            print("""Provide a whole number between "0" and "10".**** """)
     while True:
-        if attempts < min(scoreboard):
-            scoreboard.append(attempts)
-            print("NEW HIGH SCORE!!!")
-            print(f"the new high score is {min(scoreboard)}")
-        else:
-            print(f"The current high score is still {min(scoreboard)}")
-        play_again = input("would you like to tray again? Y/N ")
+        scoreboard_mean = float(mean(scoreboard))
+        scoreboard_median = float(median(scoreboard))
+        scoreboard_mode = mode(scoreboard)
+        print(
+            f"The high score statistics are | mean: {scoreboard_mean}, median: {scoreboard_median}, mode: {scoreboard_mode}\n")
+        play_again = input(">>>Would you like to tray again? Y/N <<<")
         play_again.lower()
         if play_again == "y":
             print("sure, lets get started")
             start_game()
         elif play_again == "n":
             input("thanks for playing!")
-            break
         else:
             print("try again!")
+
 
 start_game()
